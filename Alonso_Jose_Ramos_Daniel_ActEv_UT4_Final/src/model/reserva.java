2791083ce451b2c0b9ed0e76a7b2852c21eb4065
JoseAAN <joseantonio.alonsonavarro@gmail.com>
2025-03-25 20:35:59 +0000

    añadido el costructor y los getters a la clase reserva

--- Alonso_Jose_Ramos_Daniel_ActEv_UT4_Final/src/model/reserva.java
+++ Alonso_Jose_Ramos_Daniel_ActEv_UT4_Final/src/model/reserva.java
@@ -3,9 +3,30 @@ package model;
 import java.util.Date;
 
 public class reserva {
-private String idReserva;
-private Date fechaCheckIn;
-private Date fechaCheckOut;
-private double totalReserva;
 
+    private String idReserva;
+    private Date fechaCheckIn;
+    private Date fechaCheckOut;
+    private double totalReserva;
+
+    public reserva(String idReserva, Date fechaCheckin, Date fechaCheckOut, double totalReserva) {
+        this.idReserva = idReserva;
+        this.fechaCheckIn = fechaCheckin;
+        this.fechaCheckOut = fechaCheckOut;
+        this.totalReserva = totalReserva;
+    }
+
+    public String getIdReserva(){
+        return idReserva;
+    }
+
+    public Date getFechaChekIn(){
+        return fechaCheckIn;
+    }
+    public Date getFechaCheckOut(){
+        return fechaCheckOut;
+    }
+    public double getTotalReserva(){
+        return  totalReserva;
+    }
 }
