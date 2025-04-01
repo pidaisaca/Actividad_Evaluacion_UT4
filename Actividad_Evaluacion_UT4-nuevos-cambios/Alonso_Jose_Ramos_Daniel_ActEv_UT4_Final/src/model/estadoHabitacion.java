dd6ced8acf3d261ac06643a3ac72ac08d31affd6
Daniel Ramos Montoya <167306596+pidaisaca@users.noreply.github.com>
2025-04-01 19:33:02 +0100

    Delete Actividad_Evaluacion_UT4-nuevos-cambios directory

--- Actividad_Evaluacion_UT4-nuevos-cambios/Alonso_Jose_Ramos_Daniel_ActEv_UT4_Final/src/model/estadoHabitacion.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public enum estadoHabitacion {
-        DISPONIBLE,
-        RESERVADA,
-        OCUPADA
-}
--- Actividad_Evaluacion_UT4-nuevos-cambios/Alonso_Jose_Ramos_Daniel_ActEv_UT4_Final/src/view/Vista.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-public class Vista {
-
-    public static void imprimir(String mensaje){
-        System.out.println(mensaje);
-    }
-    
-}
